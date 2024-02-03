26a23cdab1b0577cd5ff65811fcc0f6f2f95aaaa
Yen Mai <110384864+yen-mai-codes@users.noreply.github.com>
2024-02-03 16:50:25 -0500

    refactored code for easier testing

---
+++ cli_solution.py
@@ -0,0 +1,57 @@
+"""
+Simple CLI solution.
+"""
+
+import argparse
+import sys
+
+def parse_args(args) -> list:
+   """
+   Parses the arguments from command line. Throws error if it's not a list of integers, or its length is not a multitude of 10.
+
+   Args:
+      list: a list of items from command line.
+
+   Returns:
+      list: a list of integers.
+   """
+   parser = argparse.ArgumentParser(description='Process a list of integers, with the items at positions which are multiple of 2 or 3 removed. Emits error if the list is not a multiple of 10 in length.')
+   parser.add_argument("-l", "--list", type=int, help="a list of integers. It should be a multiple of 10 in length.", nargs='+')
+
+   namespace = parser.parse_args(args)
+   lst = namespace.list
+
+   if (len(lst) % 10 != 0): 
+      raise parser.error("The list must be a multiple of 10 in length")
+   return lst
+
+def process_list(lst : list) -> list:
+   """
+   Return new list by removing items at positions which are a multiple of 2 or 3 in lst.
+
+   Args:
+      lst (list): a list of integers.
+
+   Returns:
+      list: the processed list.
+   """
+   
+   lst_len = len(lst)
+   new_lst = []
+
+   for i in range(lst_len):
+      if (i % 2 != 0) and (i % 3 != 0):
+         new_lst.append(lst[i])
+   return new_lst
+
+def main(): 
+   lst = parse_args(sys.argv[1:])
+   new_lst = process_list(lst)
+   print('The input list is: ', lst)
+   print('The processed list is: ', new_lst)
+   return new_lst
+
+if __name__=="__main__": 
+    main() 
+
+ 
